487ffe8273167637c1427f2fb089fcb4d9aeac64
programmer_cj <programmer_cj@163.com>
2023-03-03 01:22:56 +0800

    junit

---
+++ src/test/java/com/programmer/commons/utils/JacksonUtilTest.java
@@ -0,0 +1,16 @@
+package com.programmer.commons.utils;
+
+import org.junit.Test;
+
+/**
+ * @Author programmerGoGo
+ * @Description
+ */
+public class JacksonUtilTest {
+
+    @Test
+    public void test() {
+        System.out.println("test...");
+    }
+
+}
