d9d2a9267ef5ca6b501f59e24c26040bead8a9f1
programmer_cj <programmer_cj@163.com>
2023-03-04 00:12:02 +0800

    jacksonUtil

--- src/main/java/com/programmer/commons/utils/JacksonUtil.java
+++ src/main/java/com/programmer/commons/utils/JacksonUtil.java
@@ -1,8 +1,94 @@
 package com.programmer.commons.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author programmerGoGo
  * @Description
  */
 public class JacksonUtil {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        //设置输入时忽略JSON字符串中存在而Java对象实际没有的属性
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static String object2Json(Object o) {
+        if (o == null)
+            return null;
+
+        String s = null;
+
+        try {
+            s = mapper.writeValueAsString(o);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return s;
+    }
+
+    public static <T> List<String> listObject2ListJson(List<T> objects) {
+        if (CollectionUtils.isEmpty(objects)) {
+            return null;
+        }
+
+        List<String> lists = new ArrayList<>();
+        for (T t : objects) {
+            lists.add(object2Json(t));
+        }
+
+        return lists;
+    }
+
+    public static <T> List<T> listJson2ListObject(List<String> jsons, Class<T> c) {
+        if (CollectionUtils.isEmpty(jsons)) {
+            return null;
+        }
+
+        List<T> ts = new ArrayList<T>();
+        for (String j : jsons) {
+            ts.add(json2Object(j, c));
+        }
+
+        return ts;
+    }
+
+    public static <T> T json2Object(String json, Class<T> c) {
+        if (StringUtils.isBlank(json)) {
+            return null;
+        }
+
+        T t = null;
+        try {
+            t = mapper.readValue(json, c);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return t;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T json2Object(String json, TypeReference<T> tr) {
+        if (StringUtils.isBlank(json)) {
+            return null;
+        }
+
+        T t = null;
+        try {
+            t = (T) mapper.readValue(json, tr);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return (T) t;
+    }
 }
--- src/test/java/com/programmer/commons/utils/JacksonUtilTest.java
+++ src/test/java/com/programmer/commons/utils/JacksonUtilTest.java
@@ -1,6 +1,10 @@
 package com.programmer.commons.utils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * @Author programmerGoGo
@@ -9,8 +13,76 @@ import org.junit.Test;
 public class JacksonUtilTest {
 
     @Test
-    public void test() {
-        System.out.println("test...");
+    public void object2Json() {
+        User user = new User("zhangsan", 11);
+        System.out.println(JacksonUtil.object2Json(user)); //{"name":"zhangsan","age":11}
     }
 
+    @Test
+    public void listObject2ListJson() {
+        User user1 = new User("zhangsan", 11);
+        User user2 = new User("lisi", 12);
+        System.out.println(JacksonUtil.listObject2ListJson(Lists.newArrayList(user1, user2))); // [{"name":"zhangsan","age":11}, {"name":"lisi","age":12}]
+
+    }
+
+    @Test
+    public void listJson2ListObject() {
+        String s1 = "{\"name\":\"zhangsan\",\"age\":11}";
+        String s2 = "{\"name\":\"lisi\",\"age\":12}";
+        List<User> list = JacksonUtil.listJson2ListObject(Lists.newArrayList(s1, s2), User.class);
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void json2Object() {
+        String s1 = "{\"name\":\"zhangsan\",\"age\":11}";
+        User user = JacksonUtil.json2Object(s1, User.class);
+        System.out.println(user);
+    }
+
+    @Test
+    public void json2Object1() {
+        String s1 = "{\"name\":\"zhangsan\",\"age\":11}";
+        User user = JacksonUtil.json2Object(s1, new TypeReference<User>() {
+        });
+        System.out.println(user);
+    }
+
+    private static class User {
+        private String name;
+        private int age;
+
+        public User() {
+        }
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
 }
